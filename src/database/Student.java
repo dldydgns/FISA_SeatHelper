49f354801be70e6c0951d50d1463091aeb15db60
dldydgns <agahong1@gmail.com>
2025-07-04 15:15:48 +0900

    model.domain제거

---
+++ src/database/Student.java
@@ -0,0 +1,16 @@
+package database;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+	 String name;
+	 int age;
+	 char[] mbti;
+}
