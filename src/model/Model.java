5e0722e532ccd06023f5e67fe70f468cfae1cf61
dldydgns <agahong1@gmail.com>
2025-07-04 17:12:56 +0900

    Student Input 1차와 view 통합

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,17 +1,4 @@
 package model;
 
-import java.util.ArrayList;
-
-import database.Database;
-import database.Student;
-
 public class Model {
-	public static void main(String[] args) {
-		Database db = new Database();
-		ArrayList<Student> students = db.getAllStudents();
-		
-		for(Student st : students) {
-			System.out.println(st.getName());
-		}
-	}
 }
