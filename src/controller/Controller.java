7acb3dc1574524909c6481e8606a55cc3565c7ae
ddddabi <ii887742@gmail.com>
2025-07-04 16:57:18 +0900

    model, controller 작성완료

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,41 @@
 package controller;
 
+import database.Student;
+import model.Model;
+
+// git 올려
 public class Controller {
+    private static final Model model = Model.getModel();
+    
+    // 1. 전체 수강생 목록 가져오기
+    public static void getAllStudents() {
+        Student[] students = model.getStudents();
+
+        if (students.length != 0) {
+            for (Student stu : students) {
+                System.out.println("이름: " + stu.getName() + " / ID: " + stu.getId());
+            }
+        } else {
+            System.out.println("학생 데이터 없음");
+        }
+    }
+    
+    // 2. 랜덤 자리 출력
+    public static void printRandomSeats() {
+        int rows = 4;
+        int cols = 8;
+
+        String[][] seatArr = model.getRandomSeat(rows, cols);
+        
+        
+        // view로 이동할 부분
+        System.out.println("\n*** 랜덤 자리 배치도 ***");
+        for (int r = 0; r < seatArr.length; r++) {
+            for (int c = 0; c < seatArr[r].length; c++) {
+                System.out.printf("[%s]\t", seatArr[r][c]);
+            }
+            System.out.println();
+        }
+    }
 
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,58 @@
 package model;
 
+import database.Database;
+import database.Student;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// git 올려
 public class Model {
-	
+
+    private Database db = new Database();
+
+    private static Model model = new Model();
+
+    public static Model getModel() {
+        return model;
+    }
+
+    // 1. 전체 수강생 목록 가져오기
+    public Student[] getStudents() {
+        return db.getStudents();
+    }
+
+    // 2. 랜덤 자리 반환
+    public String[][] getRandomSeat(int rows, int cols) {
+        Student[] students = db.getStudents();
+
+        List<Student> shuffled = new ArrayList<>(Arrays.asList(students));
+        Collections.shuffle(shuffled);
+
+        String[][] seatArr = new String[rows][cols];
+        int index = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                // 4행 3열, 4행 4열은 빈자리로 고정 (인덱스: [3][2], [3][3])
+                if (r == 3 && (c == 2 || c == 3)) {
+                	seatArr[r][c] = "빈자리";
+                    continue;
+                }
+
+                if (index < shuffled.size()) {
+                	seatArr[r][c] = shuffled.get(index).getName();
+                    index++;
+                } else {
+                	seatArr[r][c] = "빈자리";  // 학생 다 채운 후 나머지도 빈자리
+                }
+            }
+        }
+
+        return seatArr;
+    }
+
 }
